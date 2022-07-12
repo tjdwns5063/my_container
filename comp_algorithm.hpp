ea0ca6fe37386d72547940ef5ef74015a2876e7a
tjdwns5063@naver.com <tjdwns5063@naver.com>
2022-07-12 12:44:56 +0900

    algorithm lexicographical_compare함수 수정

--- comp_algorithm.hpp
+++ comp_algorithm.hpp
@@ -29,24 +29,28 @@ template <class InputIterator1, class InputIterator2>
 bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
     InputIterator2 first2, InputIterator2 last2) {
         while (first1 != last1) {
-            if (*first1 == *last1 || *first1 > *first2)
+            if (first2 == last2 || *first1 > *first2)
                 return (false);
             else if (*first1 < *first2)
                 return (true);
+            ++first1;
+            ++first2;
         }
-        return (*first2 != *last2);
+        return (first2 != last2);
     }
 
 template <class InputIterator1, class InputIterator2, class Compare>
 bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
     InputIterator2 first2, InputIterator2 last2, Compare comp) {
         while (first1 != last1) {
-            if (*first1 == *last1 || !comp(*first1, *first2))
+            if (first2 == last2 || !comp(*first1, *first2))
                 return (false);
             else if (comp(*first1, *first2))
                 return (true);
+            ++first1;
+            ++first2;
         }
-        return (*first2 != *last2);
+        return (first2 != last2);
     }
 
 #endif
