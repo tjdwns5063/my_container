7a51cd790f50817719aae0d26b4cb031ab230c45
seongjunkim <tjdwns5063@naver.com>
2022-08-31 16:28:26 +0900

    refactor map logic, make map performance test

--- srcs/map_test/erase_test.cpp
+++ srcs/map_test/erase_test.cpp
@@ -1,5 +1,21 @@
 #include "map_test.hpp"
 
+template <typename MAP, typename A1, typename A2>
+void    map_erase_execute(MAP& map, A1 arg1, A2 arg2) {
+    struct timeval start;
+    struct timeval end;
+    long    micro_sec;
+    std::string map_type;
+
+    map_type = (typeid(map).name() == typeid(std::map<typename MAP::key_type, typename MAP::mapped_type>).name()) ? "STD" : "FT";
+    std::cout << "[ " << map_type  << " ]\n";
+    gettimeofday(&start, NULL);
+    map.erase(arg1, arg2);
+    gettimeofday(&end, NULL);
+    micro_sec = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+    std::cout << "micro sec: " << micro_sec << "us\n";
+}
+
 void map_erase_single_item_test() {
     std::cout << "-------------map_erase_single_item_test-----------------\n";
     std::map<int, std::string> s_m;
@@ -69,72 +85,23 @@ void map_erase_range_test() {
     std::cout << "----------------------------------\n";
 }
 
-void   large_erase_test() {
-    std::cout << "-------------map_large_erase_test-----------------\n";
+void    map_erase_performance_test() {
+    std::cout << "---------map_erase_performance_test---------\n";
 
-    std::map<int, std::string> s_m;
-    ft::map<int, std::string> m_m;
+    std::map<int, int> s_m;
+    ft::map<int, int> m_m;
 
-    m_m[42] = "lol";
+    for (int i = 0; i < 200; ++i) {
+        s_m.insert(std::make_pair(i, i + 1));
+        m_m.insert(ft::make_pair(i, i + 1));
+    }
 
-	m_m[50] = "mdr";
-	m_m[25] = "funny";
-
-	m_m[46] = "bunny";
-	m_m[21] = "fizz";
-	m_m[30] = "buzz";
-	m_m[55] = "fuzzy";
-
-	m_m[18] = "bee";
-	m_m[23] = "coconut";
-	m_m[28] = "diary";
-	m_m[35] = "fiesta";
-	m_m[44] = "hello";
-	m_m[48] = "world";
-	m_m[53] = "this is a test";
-	m_m[80] = "hey";
-
-	m_m[12] = "no";
-	m_m[20] = "idea";
-	m_m[22] = "123";
-	m_m[24] = "345";
-	m_m[27] = "27";
-	m_m[29] = "29";
-	m_m[33] = "33";
-	m_m[38] = "38";
-
-	m_m[43] = "1";
-	m_m[45] = "2";
-	m_m[47] = "3";
-	m_m[49] = "4";
-	m_m[51] = "5";
-	m_m[54] = "6";
-	m_m[60] = "7";
-	m_m[90] = "8";
-
-    m_m.preorder_traversal(m_m._root);
-    std::cout << "-------------------------\n";
-    m_m.erase(25);
-    m_m.erase(55);
-    m_m.erase(24);
-    m_m.erase(54);
-    m_m.erase(22);
-    m_m.erase(51);
-    m_m.erase(21);
-    // m_m.erase(53);
-
-    m_m.preorder_traversal(m_m._root);
-
-
-
-}
-
-void    map_erase_test() {
-    // map_erase_single_item_test();
-    // map_erase_range_test();
-    large_erase_test();
+    map_erase_execute(s_m, s_m.begin(), s_m.end());
+    map_erase_execute(m_m, m_m.begin(), m_m.end());
 }
 
 int main() {
-    map_erase_test();
+    map_erase_single_item_test();
+    map_erase_range_test();
+    map_erase_performance_test();
 }
--- srcs/map_test/map_test.hpp
+++ srcs/map_test/map_test.hpp
@@ -2,15 +2,19 @@
 # define MAP_TEST_HPP
 
 #include <map>
-#include <__tree>
 #include <iostream>
+#include <sys/time.h>
 #include "map.hpp"
 
 template <typename MAP>
 void    print_map(MAP& map) {
     typedef typename MAP::iterator  iter;
-    std::cout << "size: " << map.size() << '\n';
+    std::string map_type;
 
+    map_type = (typeid(map).name() == typeid(std::map<typename MAP::key_type, typename MAP::mapped_type>).name()) ? "STD" : "FT";
+    std::cout << "[ " << map_type  << " ]\n";
+
+    std::cout << "size: " << map.size() << '\n';
     std::cout << "[ ";
     for (iter it = map.begin(); it != map.end(); ++it) {
         std::cout << "( " << it->first << " " << it->second << " ) ";
