a723cfa7f5fde4063306a96c0120a52754557d67
tjdwns5063@naver.com <tjdwns5063@naver.com>
2022-09-01 21:21:02 +0900

    맵 높이 구하는 로직 수정중...

--- srcs/map_test/height_test.cpp
+++ srcs/map_test/height_test.cpp
@@ -2,16 +2,55 @@
 #include <iostream>
 
 int main(void) {
-    ft::map<int, int> m;
+	ft::map<int, std::string> mp;
 
-    for (int i = 5; i >= 0; --i) {
-        m.insert(ft::make_pair(i * 2 + 1, i + 1));
-    }
-    m.insert(ft::make_pair(8, 1));
-    m.insert(ft::make_pair(6, 1));
-    m.preorder_traversal(m.get_root());
+	mp[42] = "lol";
 
-    m.erase(5);
+	mp[50] = "mdr";
+	mp[25] = "funny";
+
+	mp[46] = "bunny";
+	mp[21] = "fizz";
+	mp[30] = "buzz";
+	mp[55] = "fuzzy";
+
+	mp[18] = "bee";
+	mp[23] = "coconut";
+	mp[28] = "diary";
+	mp[35] = "fiesta";
+	mp[44] = "hello";
+	mp[48] = "world";
+	mp[53] = "this is a test";
+	mp[80] = "hey";
+
+	mp[12] = "no";
+	mp[20] = "idea";
+	mp[22] = "123";
+	mp[24] = "345";
+	mp[27] = "27";
+	mp[29] = "29";
+	mp[33] = "33";
+	mp[38] = "38";
+
+	mp[43] = "1";
+	mp[45] = "2";
+	mp[47] = "3";
+	mp[49] = "4";
+	mp[51] = "5";
+	mp[54] = "6";
+	mp[60] = "7";
+	mp[90] = "8";
+
     std::cout << "-----------------------------------------\n";
-    m.preorder_traversal(m.get_root());
+    mp.erase(25); // right != NULL; left != NULL
+	mp.erase(55); // right != NULL; left != NULL
+    mp.erase(24); // right != NULL; left != NULL
+	mp.erase(54); // right != NULL; left != NULL
+    mp.erase(22); // right != NULL; left != NULL
+	mp.erase(51); // right != NULL; left != NULL
+    mp.erase(21); // right != NULL; left != NULL
+	mp.erase(53); // right != NULL; left != NULL
+
+
+    mp.preorder_traversal(mp.get_root());
 }
